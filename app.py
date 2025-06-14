08937193ab04005656968c8dcecb526eac491376
Samael Sánchez <antonio.sanchez35@hotmail.com>
2025-06-13 18:52:05 -0600

    Conexión a base de datos

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-ruta = os.path.abspath(os.path.dirname(__file__))
+from sqlalchemy import text
 
+ruta = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(ruta, 'database', 'app.db')}"
 db = SQLAlchemy(app)
+
+# Ruta para checar conexión
+@app.route('/dbcheck')
+def db_check():
+    try:
+        db.session.execute(text('SELECT 1'))  
+        return jsonify({'status': 'ok', 'message': 'Conectado con exito'}), 200
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': f'Error de conexion: {str(e)}'}), 500
 
 # RUTA SIMPLE
 @app.route('/')
